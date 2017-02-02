c6b13a8cd05465a6b75caf1ce6c82b5e049a7a38
adebos <adebos@yandex.ru>
2017-02-02 21:16:26 +0200

    guessing-game/feature: agree

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -3,7 +3,6 @@ class GuessingGame {
     	this.minValue = null;
     	this.maxValue = null;
     	this.middleValue = null;
-    	//this.tempValue = null;
     }
 
     setRange(min, max) {
@@ -12,10 +11,8 @@ class GuessingGame {
     }
 
     guess() {
-
     	this.middleValue = Math.round((this.minValue+this.maxValue)/2);
     	return this.middleValue;
-
     }
 
     lower() {
@@ -28,18 +25,3 @@ class GuessingGame {
 }
 
 module.exports = GuessingGame;
-
-/*
-function BinarySearch(t,A)       // t - искомый элемент,
-{                                // A - упорядоченный массив, в котором ищем.
-    var i = 0, j = A.length, k;
-
-    while (i < j)
-    {  k = Math.floor((i+j)/2);
-       if (t <= A[k]) j = k;
-       else i = k+1;
-    }
-
-    if (A[ i ] === t) return i;     // На выходе индекс искомого элемента.
-    else return -1;                 // Если искомого элемента нет в массиве, то -1.
-}*/
